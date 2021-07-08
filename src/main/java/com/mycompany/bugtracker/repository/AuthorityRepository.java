958638cf5278adac16cc05063464d39ab6ad8a21
Redouane Anwar <anwar.redouane@gmail.com>
2021-07-08 17:29:19 +0000

    Initial version of BugTrackerJHipster generated by generator-jhipster@7.1.0

---
+++ src/main/java/com/mycompany/bugtracker/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.mycompany.bugtracker.repository;
+
+import com.mycompany.bugtracker.domain.Authority;
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {}
